f81beeee717914abc70e95f3c31798fd02fb0e07
Joel De La Cruz <jdela1740@gmail.com>
2019-09-11 14:19:04 -0400

    Setup the website design.

---
+++ src/app/shared/components/site-sidenav/site-sidenav.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'site-sidenav',
+  templateUrl: './site-sidenav.component.html',
+  styleUrls: ['./site-sidenav.component.css']
+})
+export class SiteSidenavComponent {
+  title = 'joeldlc';
+}
---
+++ src/app/shared/components/site-sidenav/site-sidenav.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { SiteSidenavComponent } from './site-sidenav.component';
+
+@NgModule({
+  declarations: [
+    SiteSidenavComponent
+  ],
+  imports: [
+  ],
+  providers: [],
+  bootstrap: [SiteSidenavComponent]
+})
+export class SiteSidenavModule { }
