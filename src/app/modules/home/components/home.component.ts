ce93106b63c122727b562301c7f5f366e5c9ed26
Joel De La Cruz <45545585+joeldlc@users.noreply.github.com>
2019-09-12 19:38:21 -0400

    Add one way to parent binding for route changes via button on HomeComponent.

--- src/app/modules/home/components/home.component.ts
+++ src/app/modules/home/components/home.component.ts
@@ -1,13 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Output} from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
+  @Output() routeChanged = new EventEmitter<any>();
 
-  constructor() { }
-
-  ngOnInit() { }
+  setRoute(route: string) {
+    this.routeChanged.emit(route);
+    console.log("Emitted route change.");
+  }
 }
