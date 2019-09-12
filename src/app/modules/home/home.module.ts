9c418a4855759946d38c9da7aa6493e9bc4583d7
Joel De La Cruz <45545585+joeldlc@users.noreply.github.com>
2019-09-12 18:34:10 -0400

    Add navigation rows and social media icons wo/ routing setup.

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -1,12 +1,14 @@
 import {NgModule} from '@angular/core';
 
 import {HomeComponent} from './components/home.component';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
     HomeComponent,
   ],
   imports: [
+    MatButtonModule,
   ],
   providers: [],
   exports: [HomeComponent],
