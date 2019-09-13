def48302b53f9efcdad9864fbfe985e1e3f1ded0
Joel De La Cruz <jdela1740@gmail.com>
2019-09-13 11:39:27 -0400

    Add About me section to HomeComponent.

--- src/app/modules/home/components/home.component.spec.ts
+++ src/app/modules/home/components/home.component.spec.ts
@@ -2,12 +2,15 @@ import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {HomeComponent} from './home.component';
 
+import {MatCardModule} from '@angular/material/card';
+
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [MatCardModule],
       declarations: [HomeComponent],
     })
         .compileComponents();
