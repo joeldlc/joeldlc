55d582279dac168ae64774b30af062cd3909f171
Joel De La Cruz <jdela1740@gmail.com>
2019-09-14 13:48:24 -0400

    Update karma.conf.js

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -30,7 +30,8 @@ module.exports = function(config) {
     customLaunchers: {
       ChromeHeadlessNoSandbox: {
         base: 'ChromeHeadless',
-        flags: ['--no-sandbox']
+        flags: ['--no-sandbox'],
+        displayName: 'ChromeHeadlessNoSandbox'
       }
     },
     singleRun: false,
